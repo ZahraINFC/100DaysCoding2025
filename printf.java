1599f449caaa18cace7fc96337026a9708e41ddf
ZahraINFC <zahra071205@gmail.com>
2025-09-09 16:01:38 +0700

    Delete printf.java

--- printf.java
+++
@@ -1,9 +0,0 @@
-public class printf {
-    public static void main(String[] args) {
-        String nama = "zahra";
-        int umur = 19;
-        double ipk = 6.00;
-
-        System.out.printf("nama: %s, umur: %d, ipk: %.2f", nama, umur, ipk);
-    }
-}
