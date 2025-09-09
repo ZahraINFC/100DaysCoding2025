6884928cc11556787ce2891cd9a9e12ef68d53e3
ZahraINFC <zahra071205@gmail.com>
2025-09-09 16:00:43 +0700

    Penggunaan println(), print(), printf()

---
+++ printf.java
@@ -0,0 +1,9 @@
+public class printf {
+    public static void main(String[] args) {
+        String nama = "zahra";
+        int umur = 19;
+        double ipk = 6.00;
+
+        System.out.printf("nama: %s, umur: %d, ipk: %.2f", nama, umur, ipk);
+    }
+}
