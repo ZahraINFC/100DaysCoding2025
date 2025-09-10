13cab2f7f82e8a5ed6b7a353fb7eb4cecd81dc9d
ZahraINFC <zahra071205@gmail.com>
2025-09-10 17:16:27 +0700

    Memahami dan Menggunakan komentar

---
+++ Day3.java
@@ -0,0 +1,19 @@
+public class Day3 {
+    public static void main(String[] args) {
+        // menampilkan judul program
+        System.out.println("Biodata Mahasiswa");
+
+        /* menampilkan informasi dasar
+           berupa nama, umur, dan jurusan */
+
+        System.out.println("Nama     : Zahra");
+        System.out.println("Umur     : 19 tahun");
+        System.out.println("Jurusan  : Informatika");
+
+        /**
+         * bagian dokumentasi
+         * method main digunakan untuk menjalankan program
+         */
+        System.out.println("Data berhasil ditampilkan");
+    }
+}
