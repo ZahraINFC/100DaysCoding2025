418a5099291e52b4f921be439548765bcc271cc6
ZahraINFC <zahra071205@gmail.com>
2025-09-11 18:07:58 +0700

    Penggunaan Escape Sequence (\n, \t, dan lain lain)

---
+++ Day4.java
@@ -0,0 +1,31 @@
+public class Day4 {
+    public static void main(String[] args) {
+        // 1. \n = baris baru
+        System.out.println("contoh baris pertama\ncontoh baris kedua");
+
+        // 2. \t = tab
+        System.out.println("nama:\tzahra");
+        System.out.println("umur:\t19");
+
+        // 3. \\ = backslash
+        System.out.println("folder: C:\\Users\\zahra");
+
+        // 4. \" = tanda kutip ganda
+        System.out.println("teman bilang: \"ayo belajar java bareng\"");
+
+        // 5. \' = tanda kutip tunggal
+        System.out.println("karakter ini: \'Z\'");
+
+        // 6. \r = carriage return
+        System.out.println("halo\rzahaa");
+
+        // 7. \b = backspace
+        System.out.println("helloo\b!");
+
+        // 8. \f = form feed
+        System.out.println("ini sebelum\fini sesudah");
+
+        // 9. \u2605 = simbol bintang (★)
+        System.out.println("simbol bintang: \u2605");
+    }
+}
