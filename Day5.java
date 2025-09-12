5ababd6157fb8a841eb2a794b9332532281279d5
ZahraINFC <zahra071205@gmail.com>
2025-09-12 17:34:20 +0700

    Create Day5.java

---
+++ Day5.java
@@ -0,0 +1,14 @@
+public class Day5 {
+    public static void main(String[] args) {
+
+        byte umur = 19;
+        short tahun = 2025;
+        int gaji = 5000000;
+        long populasi = 7800000000L;
+
+        System.out.println("umur: " + umur);
+        System.out.println("tahun: " + tahun);
+        System.out.println("gaji: " + gaji);
+        System.out.println("populasi dunia: " + populasi);
+    }
+}
