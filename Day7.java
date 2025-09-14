b46564fea94da739ad63db7fef38858635e9851a
ZahraINFC <zahra071205@gmail.com>
2025-09-14 20:07:33 +0700

    Tipe Data Karakter (char) dan Boolean (boolean)

---
+++ Day7.java
@@ -0,0 +1,13 @@
+public class Day7 {
+        public static void main(String[] args) {
+            // char
+            char hurufAwal = 'R';
+
+            // boolean
+            boolean lapar = false;
+
+            System.out.println("Huruf awal nama: " + hurufAwal);
+
+            System.out.println("Apakah aku sedang lapar? " + lapar);
+        }
+}
