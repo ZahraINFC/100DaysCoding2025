ae3a1c825c50f81f36471a47b3da9b02aadfacce
ZahraINFC <zahra071205@gmail.com>
2025-09-15 11:40:41 +0700

    Pengenalan Tipe Data String untuk teks

---
+++ Day8.java
@@ -0,0 +1,7 @@
+public class Day8 {
+    public static void main(String[] args) {
+        //Tipe data string untuk teks
+        String kalimat = "saya suka menonton";
+        System.out.println(kalimat);
+    }
+}
