867fac1f4a32d24067d3daa05c17c7109f87244e
ZahraINFC <zahra071205@gmail.com>
2025-09-16 21:09:48 +0700

    Deklarasi, Inisialisasi dan Update Variabel.

---
+++ Day9.java
@@ -0,0 +1,10 @@
+public class Day9 {
+    public static void main(String[] args) {
+        //deklarasi
+        String nama;
+        //inisialisasi
+        String nim = "D0224023";
+        //update
+        nim = "D0224024";
+    }
+}
