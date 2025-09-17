6b7cb11c134b84b354fae5f34957fbdb0cc173e1
ZahraINFC <zahra071205@gmail.com>
2025-09-17 18:54:54 +0700

    Constanta (Final Variabel)

---
+++ Day10.java
@@ -0,0 +1,8 @@
+public class Day10 {
+    public static void main(String[] args) {
+        // konstanta adalah variabel yang nilainya tetap (tidak bisa diubah)
+        // contoh konstanta
+        final double tb = 150;
+        System.out.println("Nilai konstanta PI adalah: " + tb);
+    }
+}
