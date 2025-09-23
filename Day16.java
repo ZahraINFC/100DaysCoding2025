bf6c8660ff7e8838e8a517960cbac02107f479c7
ZahraINFC <zahra071205@gmail.com>
2025-09-23 18:12:26 +0700

    Operator Aritmatika Perkalian (*) dan Pembagian (/)

---
+++ Day16.java
@@ -0,0 +1,16 @@
+public class Day16{
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 5;
+        // perkalian
+        int hasilkali = a * b;
+
+        // pembagian
+        int hasilbagi = a / b;
+
+        // hasil
+        System.out.println("Hasil Perkalian = " + hasilkali);
+        System.out.println("Hasil Pembagian = " + hasilbagi);
+    }
+}
+
