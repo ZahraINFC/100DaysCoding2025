539841358b7973ad272a38265a60fc83410834d6
ZahraINFC <zahra071205@gmail.com>
2025-09-24 18:40:00 +0700

    Operator Aritmatika Modulus atau Sisa Bagi (%)

---
+++ Day17.java
@@ -0,0 +1,13 @@
+public class Day17 {
+    public static void main(String[] args) {
+        
+        int a = 20;
+        int b = 6;
+
+        // modulus (sisa bagi)
+        int hasilModulus = a % b;
+
+        // hasil
+        System.out.println("Hasil Modulus = " + hasilModulus);
+    }
+}
