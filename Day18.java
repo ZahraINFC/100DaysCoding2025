8ac71540e009ed2a43bb345a7bf6ad75a6f66b5d
ZahraINFC <zahra071205@gmail.com>
2025-09-25 14:29:39 +0700

    Konversi Otomatis (Dari Tipe Data Kecil ke Besar).

---
+++ Day18.java
@@ -0,0 +1,9 @@
+public class Day18 {
+    public static void main(String[] args) {
+        int angkaInt = 50;     // tipe data int
+        double angkaDouble = angkaInt; // jadi double
+
+        System.out.println("nilai int: " + angkaInt);
+        System.out.println("nilai double: " + angkaDouble);
+    }
+}
