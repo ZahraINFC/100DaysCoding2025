99e4af2a963dd4c39cccd644a5a6fcfb431fd1de
ZahraINFC <zahra071205@gmail.com>
2025-09-26 20:34:58 +0700

    Konversi Manual/Paksa (Dari Tipe Data Besar ke Kecil)

---
+++ Day19.java
@@ -0,0 +1,11 @@
+public class Day19 {
+    public static void main(String[] args) {
+        double angkaDouble = 9.8;   // tipe data besar (64 bit)
+
+        // konversi manual dari double ke int
+        int angkaInt = (int) angkaDouble;
+
+        System.out.println("nilai double: " + angkaDouble);
+        System.out.println("nilai int: " + angkaInt);
+    }
+}
