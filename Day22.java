ee286b90c0ed9c671de419c8a37466c3d10659ff
ZahraINFC <zahra071205@gmail.com>
2025-09-29 18:47:39 +0700

    Latihan : Program Luas Persegi.

---
+++ Day22.java
@@ -0,0 +1,9 @@
+public class Day22 {
+    public static void main(String[] args) {
+        int sisi = 5;
+        int luas = sisi * sisi;
+
+        System.out.println("Sisi persegi = "+ sisi);
+        System.out.println("Luas persegi = "+ luas);
+    }
+}
