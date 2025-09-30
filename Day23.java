cd98ec0667ebd1e5b0615e4df3e2b18c0baadb3e
ZahraINFC <zahra071205@gmail.com>
2025-09-30 20:50:29 +0800

    Latihan: Program Luas Persegi Panjang.

---
+++ Day23.java
@@ -0,0 +1,12 @@
+public class Day23 {
+    public static void main(String[] args) {
+        int panjang = 8; 
+        int lebar = 5; 
+
+        int luas = panjang * lebar;
+
+        System.out.println("Panjang: " + panjang);
+        System.out.println("Lebar: " + lebar);
+        System.out.println("Luar persegi panjang: " + luas);
+    }
+}
