4bb72c0f9f0f7613acc77922481db7ca07cffed7
ZahraINFC <zahra071205@gmail.com>
2025-10-01 14:38:41 +0700

    Latihan: Program Luas Lingkaran.

---
+++ Day24.java
@@ -0,0 +1,9 @@
+public class Day24 {
+    public static void main(String[] args) {
+        double jariJari = 7;
+        double luas = 3.14 * jariJari * jariJari;
+
+        System.out.println("Jari-jari: " + jariJari);
+        System.out.println("Luas lingkaran: " + luas);
+    }
+}
