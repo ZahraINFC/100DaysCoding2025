ab23fc42f05ce88d773af06c9025744315510e17
ZahraINFC <zahra071205@gmail.com>
2025-10-02 20:01:02 +0800

    Operator Increment dan Decrement (++, --)

---
+++ Day25.java
@@ -0,0 +1,11 @@
+public class Day25 {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 6 ; i++) {
+            System.out.println("nilai i : " + i);
+        }
+        System.out.println("");
+        for (int j = 6; j >= 1; j--) {
+            System.out.println("nilai j : " + j);
+        }
+    }
+}
