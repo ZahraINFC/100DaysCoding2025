d6ba085f7f77b4656eb565b1b596335b6ed4ee5e
ZahraINFC <zahra071205@gmail.com>
2025-10-03 21:52:07 +0800

    Operator Penugasan (=, +=, -=, dll).

---
+++ Day26.java
@@ -0,0 +1,21 @@
+public class Day26 {
+    public static void main(String[] args) {
+        int a = 10;  // operator =
+        System.out.println("nilai awal a = " + a);
+
+        a += 5; // sama dengan a = a + 5
+        System.out.println("setelah a += 5, nilai a = " + a);
+
+        a -= 3; // sama dengan a = a - 3
+        System.out.println("setelah a -= 3, nilai a = " + a);
+
+        a *= 2; // sama dengan a = a * 2
+        System.out.println("setelah a *= 2, nilai a = " + a);
+
+        a /= 4; // sama dengan a = a / 4
+        System.out.println("setelah a /= 4, nilai a = " + a);
+
+        a %= 3; // sama dengan a = a % 3
+        System.out.println("setelah a %= 3, nilai a = " + a);
+    }
+}
