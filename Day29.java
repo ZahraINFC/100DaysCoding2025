0a1130372e497844786d631b699657dc9d2ce1c2
ZahraINFC <zahra071205@gmail.com>
2025-10-06 21:40:28 +0700

    Operator Perbandingan < dan >.

---
+++ Day29.java
@@ -0,0 +1,9 @@
+public class Day29 {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 7;
+
+        System.out.println("a > b : " + (a > b));
+        System.out.println("a < b : " + (a < b));
+    }
+}
