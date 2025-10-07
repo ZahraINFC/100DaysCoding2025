dc80788944d8a0fe478c815994d87215895353f9
ZahraINFC <zahra071205@gmail.com>
2025-10-07 21:05:20 +0700

    Operator Perbandingan <= dan >=.

---
+++ Day30.java
@@ -0,0 +1,12 @@
+public class Day30 {
+    public static void main(String[] args) {
+
+        int x = 15;
+        int y = 12;
+        int z = 15;
+
+        System.out.println(x <= y); // false 
+        System.out.println(x >= y); // true 
+        System.out.println(x <= z); // true 
+    }
+}
