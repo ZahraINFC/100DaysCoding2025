210b68b3ae6b8bad1df630c81113ae5aef760773
ZahraINFC <zahra071205@gmail.com>
2025-10-10 19:29:28 +0700

    Operator Logika NOT (!).

---
+++ Day33.java
@@ -0,0 +1,10 @@
+public class Day33 {
+    public static void main(String[] args) {
+        // operator logika NOT (!) digunakan untuk membalikkan nilai boolean
+
+        boolean status = true;
+
+        System.out.println(status);   // true
+        System.out.println(!status);  // false
+    }
+}
