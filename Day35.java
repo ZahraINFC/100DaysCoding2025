fb2efe0d9f2815ea5227f10fb4d82da94aa5dcc2
ZahraINFC <zahra071205@gmail.com>
2025-10-12 22:09:46 +0700

    Percabangan

---
+++ Day35.java
@@ -0,0 +1,11 @@
+public class Day35 {
+    public static void main(String[] args) {
+        int nilai = 60;
+
+        if (nilai >= 70) {
+            System.out.println("Kamu lulus!");
+        } else {
+            System.out.println("Kamu belum lulus!");
+        }
+    }
+}
