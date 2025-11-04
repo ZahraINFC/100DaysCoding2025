5ea673435579f7a43a8095dd5cbc13569b6a432e
ZahraINFC <zahra071205@gmail.com>
2025-11-04 16:13:05 +0700

    Latihan : Mencetak angka 1-N dan angka N-1

--- Day58.java
+++ Day58.java
@@ -1,1 +1,22 @@
+import java.util.Scanner;
+public class Day58 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("masukkan nilai N: ");
+        int n = input.nextInt();
+
+        System.out.println("\nangka dari 1 sampai " + n + ":");
+        for (int i = 1; i <= n; i++) {
+            System.out.print(i + " ");
+        }
+
+        System.out.println("\n\nangka dari " + n + " sampai 1:");
+        for (int i = n; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+
+        input.close();
+    }
+}
 
