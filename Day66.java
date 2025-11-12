9be65acc10188d4b87149523456c48875c4e0570
ZahraINFC <zahra071205@gmail.com>
2025-11-12 22:11:17 +0700

    Pola Vertikal

---
+++ Day66.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class Day66 {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.println("Masukkan baris : ");
+        int n = s.nextInt();
+        for (int i = 0; i < n; i++) System.out.println("*");
+    }
+}
