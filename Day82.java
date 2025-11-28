cb2cff4c448153dec43e7975052872333aca9108
ZahraINFC <zahra071205@gmail.com>
2025-11-28 22:07:56 +0800

    Mengisi elemen array dan Menampilkan menggunakan perulangan

---
+++ Day82.java
@@ -0,0 +1,10 @@
+public class Day82 {
+    public static void main(String[] args) {
+        int[] brg = {"mangga, "apel", "kucing", "celana", " kayu", "freshcare"};
+        for (int i = 0; i <= brg.length ; i++) {
+
+            System.out.println(brg[i]);
+        }
+
+    }
+}
