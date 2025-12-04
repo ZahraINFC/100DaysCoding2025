339995fe01926cf86ba41cd7ab9e0a4bd9d9e4fc
ZahraINFC <zahra071205@gmail.com>
2025-12-04 20:42:02 +0800

    Mengakses elemen Array menggunakan for-each

---
+++ Day88.java
@@ -0,0 +1,10 @@
+public class Day88 {
+    public static void main(String[] args) {
+        String[] buah = {"Apel", "Jeruk", "Mangga", "Pisang"};
+
+        System.out.println("Daftar buah:");
+        for (String b : buah) {
+            System.out.println(b);
+        }
+    }
+}
