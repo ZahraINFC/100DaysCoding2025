d6213d4cd1daaeb9c2e54a9befda0f846849c7a2
ZahraINFC <zahra071205@gmail.com>
2025-12-05 21:25:48 +0800

    Method Void

---
+++ Day89.java
@@ -0,0 +1,15 @@
+public class Day89 {
+
+    public static void salam() {
+        System.out.println("Selamat datang di program Java!");
+    }
+
+    public static void cetakNama(String nama) {
+        System.out.println("Nama: " + nama);
+    }
+
+    public static void main(String[] args) {
+        salam();
+        cetakNama("Zahra");
+    }
+}
