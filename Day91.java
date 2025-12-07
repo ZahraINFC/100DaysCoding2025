a796b680f4efb1f0f45f8fdb827e5295262c8ecf
ZahraINFC <zahra071205@gmail.com>
2025-12-07 21:24:58 +0700

    Parameter method

---
+++ Day91.java
@@ -0,0 +1,11 @@
+public class Day91 {
+
+    public static void cetakUmur(String nama, int umur) {
+        System.out.println(nama + " berumur " + umur + " tahun.");
+    }
+
+    public static void main(String[] args) {
+        cetakUmur("Zahra", 19);
+        cetakUmur("Alya", 20);
+    }
+}
