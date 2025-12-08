2f3662719751a48f17a3d842f758dbfc54ee0c88
ZahraINFC <zahra071205@gmail.com>
2025-12-08 18:18:22 +0800

    Keyword return

---
+++ Day92.java
@@ -0,0 +1,9 @@
+public class Day92 {
+    public static int luasPersegi(int sisi) {
+        return sisi * sisi;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Luas: " + luasPersegi(5));
+    }
+}
