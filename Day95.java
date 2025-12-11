71582bafd848b45bdd0d09f949655ea7362553f5
ZahraINFC <zahra071205@gmail.com>
2025-12-11 20:15:58 +0800

    Mencetak dengan Parameter void

---
+++ Day95.java
@@ -0,0 +1,9 @@
+public class Day95 {
+     static void cetakNama(String nama) {
+        System.out.println("Halo " + nama + "!");
+    }
+
+    public static void main(String[] args) {
+               cetakNama("Zahra");
+    }
+}
